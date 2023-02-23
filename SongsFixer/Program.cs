779766254d8e5768ba2c9fff4cb973c2e45aa03c
AnAlgoTrader <an.algo.trader@gmail.com>
2023-02-23 16:35:20 +0100

    .

--- SongsFixer/Program.cs
+++ SongsFixer/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace SongsFixer
+{
+    public class Program
+    {
+        private static string songsDirectory = "../songs";
+        static void Main()
+        {
+            var songToFix = File.ReadAllText($"{songsDirectory}/template.html");
+            var doc = new HtmlDocument();
+            doc.LoadHtml(songToFix);
+            RemoveClassAttributes(doc);
+            ChangeChordColors(doc);
+            FileStream sw = new FileStream($"{songsDirectory}/fixed.html", FileMode.Create);
+            doc.Save(sw);
+        }
+
+        private static void ChangeChordColors(HtmlDocument html)
+        {
+            var elements = html.DocumentNode.SelectNodes("//span");
+            if (elements != null)
+            {
+                foreach (var element in elements)
+                {
+                    if (element.Attributes.Contains("style"))
+                    {
+                        element.Attributes["style"].Value = element.Attributes["style"].Value.Replace("rgb(0, 0, 0)", "orangered");
+                    }
+                }
+            }
+        }
+
+        public static void RemoveClassAttributes(HtmlDocument html)
+        {
+            var elements = html.DocumentNode.SelectNodes("//@class");
+
+            if (elements != null)
+            {
+                foreach (var element in elements)
+                {
+                    element.Attributes["class"].Remove();
+                }
+            }
+        }
+    }
+}
