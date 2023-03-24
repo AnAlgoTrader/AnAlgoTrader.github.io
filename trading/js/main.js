db96ac77ee316958f2ad900698e9cdca2edef820
AnAlgoTrader <an.algo.trader@gmail.com>
2023-03-24 09:09:27 +0100

    .

--- trading/js/main.js
+++ trading/js/main.js
@@ -1,3 +1,11 @@
+
+
+
+
+//-------------------------------------------------------------------------------------------------
+// Connection to server
+//-------------------------------------------------------------------------------------------------
+
 function getRequests() {
     var s1 = location.search.substring(1, location.search.length).split('&'),
         r = {}, s2, i;
@@ -9,6 +17,43 @@ function getRequests() {
 };
 
 var queryString = getRequests();
-console.log(queryString["i"]);
-console.log(queryString["u"]);
-console.log(queryString["p"]);
+const ip = queryString["i"];
+const u = queryString["u"];
+const p = queryString["p"];
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl(`http://${ip}:5556/mainHub`, { accessTokenFactory: async() => await getToken() })
+    .withAutomaticReconnect()
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+connection.on("DataChannel", (sender, type, message) => {
+    if (sender == "IB.Trader") {
+    }
+});
+
+async function getToken() {
+    const rawResponse = await fetch(`http://${ip}:5556/api/login`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ Username: u, Password: p })
+    });
+    return await rawResponse.json();
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("Client side is connected and ready.");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+connection.onclose(async () => {
+    await start();
+});
+start();
