14ca34403be95d1a3485ac583cb8dfacd5d629ae
AnAlgoTrader <an.algo.trader@gmail.com>
2023-09-22 19:59:56 +0200

    .

--- replay/scripts/constants.js
+++
@@ -1,6 +0,0 @@
-var green = 'rgb(38,166,154)';
-var red = 'rgb(255,82,82)';
-var black = '#000000';
-var white = 'rgba(255, 255, 255, 0.9)';
-var grey = 'rgba(42, 46, 57, 0.5)';
-var yellow = 'rgba(228, 228, 70, 0.8)';
